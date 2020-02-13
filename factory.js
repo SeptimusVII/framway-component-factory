725fbd2b071dce0a23be72d90e756b5111824156
Quentin Vansteene <vansteene.quentin@gmail.com>
2020-02-13 16:31:43 +0100

    Initial commit

---
+++ factory.js
@@ -0,0 +1,10 @@
+var Factory = Object.getPrototypeOf(app).Factory = new Component("factory");
+Factory.debug = true;
+Factory.createdAt      = "2.0.0";
+Factory.lastUpdate     = "2.0.0";
+Factory.version        = "1";
+// Factory.loadingMsg     = "This message will display in the console when component will be loaded.";
+
+// Factory.prototype.onCreate = function(){
+  // do thing after element's creation
+// }
