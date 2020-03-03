1069c4b04b81a47185a03f57932a285fdc873083
Quentin Vansteene <vansteene.quentin@gmail.com>
2020-03-03 17:13:29 +0100

    add basic functionalities:  - select components from the active configuration  - component preview  - component text editor  - component prebuild configuration

--- factory.js
+++ factory.js
@@ -1,10 +1,194 @@
 var Factory = Object.getPrototypeOf(app).Factory = new Component("factory");
-Factory.debug = true;
+// Factory.debug = true;
 Factory.createdAt      = "2.0.0";
 Factory.lastUpdate     = "2.0.0";
 Factory.version        = "1";
 // Factory.loadingMsg     = "This message will display in the console when component will be loaded.";
 
-// Factory.prototype.onCreate = function(){
-  // do thing after element's creation
-// }
+Factory.prototype.onCreate = function(){
+	var factory = this;
+	factory.$select = $('<select class="factory__select"><option value>Select a component</option></select>');
+	factory.$sampler = $('<div class="factory__sampler"></div>');
+	factory.$editor = $('<div class="factory__editor hidden"><button class="copy">Copy</button><textarea></textarea></div>');
+	factory.$constructor = $('<div class="factory__constructor"></div>');
+
+	if (config.components.length > 1) {
+		for(var item of config.components)
+			if (item != 'factory')
+				factory.$select.append('<option value="'+item+'">'+item+'</option>');
+		factory.$el.append(factory.$select);
+		factory.$el.append(factory.$sampler);
+		factory.$el.append(factory.$editor);
+		factory.$el.append(factory.$constructor);
+	} else {
+		factory.$el.append('<p class="error">No component available</p>');
+	}
+
+	factory.$select.on('change',function(){
+		if ($(this).val() != ''){
+			var component = $('<div>'+require('html-loader!../'+factory.$select.val()+'/sample.html')+'</div>');
+			factory.$editor.removeClass('hidden');
+			if (component.find('constructor').length)
+				factory.$constructor.html(getConstructor(component.find('constructor').remove()));
+			else
+				factory.$constructor.html('');
+			factory.$editor.find('textarea').val(component.get(0).innerHTML);
+			factory.$constructor.find('.select,.number').trigger('change');
+			factory.$constructor.find('.checkbox').each(function(){
+				if($(this).data('selected'))
+	        		$(this).trigger('click');
+			});
+			factory.$editor.find('textarea').trigger('change',true);
+		}
+		else{
+			factory.$editor.find('textarea').val('').trigger('change',true);
+			factory.$editor.addClass('hidden');
+			factory.$constructor.html('');
+		}
+	});
+
+    var timerEdit,timerEditValue;
+	var editorText = factory.$editor.find('textarea').val();
+	factory.$editor.find('textarea').on('change keyup',function(e,forced){
+		console.log('textarea forced',forced);
+		timerEditValue = 500;
+		if(forced) 
+			timerEditValue = 0;
+		clearTimeout(timerEdit);
+		timerEdit = setTimeout(function(){
+			var val = factory.$editor.find('textarea').val();
+			if(val != editorText || forced){
+				editorText = val;
+				factory.$sampler.html(val);
+			}
+		},timerEditValue);
+	});
+	factory.$editor.find('.copy').on('click',function(){
+		var elem = $(this).parent().find('textarea').get(0);
+     	if(utils.copyToClipboard(elem))
+        	notif_fade.success('Copied to clipboard !');
+	});
+
+
+	// constructor events
+  	$('body').on('change','.factory__constructor .select',function(e){
+  		factory.applyConstructorChanges($(this));
+  	});
+  	$('body').on('click','.factory__constructor .checkbox',function(e){
+  		factory.applyConstructorChanges($(this));
+  	});
+  	$('body').on('change','.factory__constructor .number',function(e){
+  		factory.applyConstructorChanges($(this));
+  	});
+
+
+	// tests
+	factory.$select.find('option').last().get(0).selected = true;
+	factory.$select.trigger('change');
+}
+
+Factory.prototype.applyConstructorChanges = function($input){
+	var factory = this;
+
+	var $editor  = factory.$editor.find('textarea');
+	var selector = $input.attr('name').split(',')[0];
+	var attr     = $input.attr('name').split(',')[1];
+    var dummy    = $($editor.val()).wrapAll('<div></div>');
+
+    var match = false;
+    var value = $input.val();
+	if ($input.hasClass('select')) {
+		match = [];
+      	$input.find('option').each(function(){
+       		if(this.value != '')
+         		match.push(this.value);
+      	});
+     	match = match.join(' ');
+	} else if($input.hasClass('checkbox') && value == "undefined"){
+      value = $input.isChecked();
+    }
+    else if($input.hasClass('checkbox') && value != "undefined" && attr != "class"){
+      if($input.isChecked()) value = $input.val();
+      else value = '';
+    } else if($input.hasClass('number')){
+    	match = [];
+    	var prefix = $input.data('prefix') || '';
+      	for (var i = $input.attr('min'); i <= $input.attr('max'); i++) 
+        	match.push(prefix+i);
+     	match = match.join(' ');
+      	if($input.val())
+        	value = prefix+$input.val();
+    }
+
+	if(attr == 'class'){
+      	if(match)
+        	dummy.parent().find('.'+selector).removeClass(match);
+      	dummy.parent().find('.'+selector).toggleClass(value);
+    } else {
+      	dummy.parent().find('.'+selector).attr(attr,value);
+    }
+
+    $editor.val(dummy.parent().get(0).innerHTML).trigger('keyup');
+};
+
+var arrSpecials = ['#colors','#breakpoints'];
+var getConstructor = function(constructor){
+	var result = '';
+	constructor.find('field').each(function(){
+		var field = {};
+		var inputGroup = '';
+		for(var attr of $(this).get(0).attributes)
+			field[attr.name] = attr.value;
+		field.id = constructor.attr('component') +','+ field.target +','+ field.label.replace(/ /g,'-').toLowerCase();
+		
+		for(var special of arrSpecials){
+			if (field.values && field.values.indexOf(special) != -1)
+				field.values = getSpecials(special,field.values)
+			if (field.labels && field.labels.indexOf(special) != -1)
+				field.labels = getSpecials(special,field.labels)
+		}
+
+		if (field.type!='separator'){
+			inputGroup += '<div class="form-group">';
+			if (field.helper && field.helper != ''){
+				inputGroup += '<i class="factory__helper__icon fas fa-question-circle"></i>';
+              	inputGroup += '<div class="factory__helper__text">'+field.helper+'</div>';
+			}
+		}
+		switch(field.type){
+			case 'separator': 
+				inputGroup += '<p class="sep-bottom">'+field.label+'</p>';
+			break;
+			case 'select': 
+				inputGroup += '<label for="'+field.id+'">'+field.label+'</label>'
+							+ '<select class="'+field.type+'" name="'+field.id+'" id="'+field.id+'">'
+							+ '<option value=""> - </option>';
+				for(var i in field.values.split(','))
+					inputGroup += '<option value="'+(field.prefix?field.prefix:'')+field.values.split(',')[i]+'"'+(field.selected == field.values.split(',')[i]?'selected':'')+'>'+field.labels.split(',')[i]+'</option>';
+				inputGroup += '</select>';
+			break;
+			case 'checkbox':
+				inputGroup += '<input type="checkbox" value="'+(field.prefix?field.prefix:'')+field.value+'" class="'+field.type+'" name="'+field.id+'" id="'+field.id+'" data-selected="'+field.selected+'" />'
+							+ '<label for="'+field.id+'">'+field.label+'</label>'
+			break;
+			case 'number':
+				var range = field.range.split('-');
+				inputGroup += '<label for="'+field.id+'">'+field.label+'</label>'
+							+ '<input type="number" min="'+range[0]+'" max="'+range[1]+'" '+(field.prefix?'data-prefix="'+field.prefix+'"':'')+' class="'+field.type+'" name="'+field.id+'" id="'+field.id+'" value="'+field.value+'" />'
+			break;
+			default: break;
+		}
+		if (field.type!='separator')
+			inputGroup += '</div>';
+		result += inputGroup;
+	});
+	return result;
+}
+
+var getSpecials = function(special,base){
+	var result = [];
+	base = base.replace(special,'');
+	for(var item in app.styles[special.replace('#','')])
+		result.push(base+item);
+	return result.join(',');
+}
